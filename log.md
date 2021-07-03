c0f5fdd2fe223b278c15aefabec38f46dffcffeb
Arturo Caliandro <73656521+caliandro-arturo@users.noreply.github.com>
2021-07-03 16:46:42 +0200

    feat(RB): add implementation for RB tree
    - add log file

---
+++ log.md
@@ -0,0 +1,78 @@
+## 3 Luglio
+
+### Descrizione del programma:
+
+Essenzialmente, il programma stipula una classifica limitata di grafi pesati diretti, inseriti in quantità
+non limitata.
+
+Un grafo pesato mette un valore, detto peso, sugli archi che collegano i nodi.
+
+Un grafo diretto pone una direzione per ogni arco, che quindi può essere uscente o entrante rispetto a un 
+nodo.
+
+Si considerano grafi connessi.
+
+I pesi rientrano nel dominio D = [0, 2^32 - 1], dove 0 rappresenta la non presenza dell'arco.
+
+Il programma riceve ingressi nel modo seguente:
+
+- `DIM K`: questi sono interi utili per inizializzare il programma
+  - DIM = dimensione dei grafi considerati;
+  - K = lunghezza della classifica.  
+- `AggiungiGrafo`: avvia la scansione di un grafo descritto secondo la matrice di adiacenza, che va
+  espressa mettendo DIM numeri separati con virgola in DIM righe. 
+  Es (DIM = 3):
+
+    > 3,4,0
+  > 
+    > 5,12,3
+  > 
+    > 6,8,9
+  
+    dove:
+    - la riga x-esima descrive la stella del nodo x (gli archi che escono);
+    - la colonna x-esima descrive gli archi entranti nel nodo x.
+    
+- `TopK`: stampa la classifica dei primi K grafi inseriti dall'avvio del programma fino al comando, 
+  mettendoli su una riga, indicandoli attraverso il loro indice (a partire da 0) e separandoli con spazio.
+
+La metrica dei grafi viene stipulata in tale modo:
+
+- si parte dal nodo 0;
+- si calcola il cammino minimo da 0 a ognuno dei DIM - 1 nodi rimanenti;
+- si sommano i cammini ottenuti.
+
+La classifica si ottiene mettendo al primo posto il grafo con il valore più piccolo della seguente metrica,
+inserito prima di ogni altro grafo che abbia la stessa metrica.
+
+### Idee
+
+#### Memoria
+
+Del grafo importa soltanto la metrica: una volta calcolata la metrica, il resto del grafo può
+essere immediatamente deallocato.
+
+La classifica contiene al più K elementi. Se K è grande ma vi sono pochi grafi inseriti, potrebbe non
+essere richiesto l'allocazione di K celle.
+
+#### Struttura
+
+L'assenza di pesi negativi induce a considerare l'algoritmo di Dijkstra. Il problema, tuttavia, è quello
+di calcolare la somma dei cammini minimi, non tanto quello di tenere memoria del percorso stesso.
+Forse le due cose sono coniugabili attraverso una semplificazione dell'algoritmo, forse vale la pena trovare
+un'altra soluzione.
+
+La classifica è una collezione ordinata ascendente senza ripetizioni, ogni grafo con la stessa metrica di
+un grafo già presente nella classifica va escluso. Questo porta a ragionare su tre problemi:
+
+- ricerca di un elemento nella collezione;
+- inserimento nella collezione, nel caso in cui la ricerca precedente abbia avuto esito negativo;
+- eliminazione di un elemento dalla collezione, qualora questa dovesse aver già raggiunto la dimensione K.
+
+Un albero binario è noto per avere buona complessità temporale - O(log n) - ma vale la pena capire se il
+sistema di reintroduzione sia altrettanto efficace.
+
+### Idea attuale di progetto
+
+Provo a usare Dijkstra per calcolare la metrica e uso un albero binario (un RB che già possiedo dai residui
+del tentativo di progetto dell'anno scorso).
--- main.c
+++ main.c
@@ -1,11 +1,280 @@
 /**
- * Progetto di API 2020-2021
- * Autore: Arturo Caliandro
+ * API project 2020-2021
+ * Author: Arturo Caliandro
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int graphDimension;
+int rankingLength;
+int actualRankingSize = 0;
+
+typedef struct treeNode {
+    int graphId;
+    int key;
+    bool isRed;
+    struct treeNode *father;
+    struct treeNode *left;
+    struct treeNode *right;
+} rankingNode;
+
+/*typedef struct {
+
+} graphNode;*/
+
+/*typedef struct {
+    int id;
+} graph;*/
+
+/* Node that represents a null value. */
+rankingNode *nil;
+/* Node that represents the root of the ranking tree. */
+rankingNode *root;
+
+/* Returns the lowest value. */
+int minInt(int val1, int val2) {
+    if (val1 <= val2)
+        return val1;
+    else return val2;
+}
+
+/* Returns the higher value. */
+int maxInt(int val1, int val2) {
+    if (val1 >= val2)
+        return val1;
+    else return val2;
+}
+
+/* Returns the node containing the required key.
+ *
+ * T: the father to consider to do the research
+ * key: the value that identifies the searched node */
+rankingNode *search(rankingNode *T, int key) {
+    if (T == nil || T->key == key)
+        return T;
+    else if (T->key < key)
+        return search(T->right, key);
+    else return search(T->left, key);
+}
+
+/* Returns the node with the lowest key, starting from a specified node. */
+rankingNode *minNode(rankingNode *subTree) {
+    rankingNode *current = subTree;
+    while (current->left != nil)
+        current = current->left;
+    return current;
+}
+
+/* Returns the node with the lowest key value that is higher than the node passed as parameter. */
+rankingNode *successor(rankingNode *startNode) {
+    if (startNode == nil)
+        return nil;
+    if (startNode->right != nil)
+        return minNode(startNode->right);
+    rankingNode *startNodeFather = startNode->father;
+    while (startNodeFather != nil && startNodeFather->right == startNode) {
+        startNode = startNodeFather;
+        startNodeFather = startNodeFather->father;
+    }
+    return startNodeFather;
+}
+
+/* Applies a counterclockwise rotation between a node and his right son.
+ * The son becomes the father of the node, and the node becomes his left son. */
+void leftRotate(rankingNode *toRotate) {
+    rankingNode *rightSon = toRotate->right;
+    toRotate->right = rightSon->left;
+    if (rightSon->left != nil)
+        rightSon->left->father = toRotate;
+    rightSon->father = toRotate->father;
+    if (toRotate->father == nil)
+        root = rightSon;
+    else if (toRotate == toRotate->father->left)
+        toRotate->father->left = rightSon;
+    else toRotate->father->right = rightSon;
+    rightSon->left = toRotate;
+    toRotate->father = rightSon;
+}
+
+/* Applies a clockwise rotation between a node and his left son.
+ * The son becomes the father of the node, and the node becomes his right son. */
+void rightRotate(rankingNode *toRotate) {
+    rankingNode *leftSon = toRotate->left;
+    toRotate->left = leftSon->right;
+    if (leftSon->right != nil)
+        leftSon->right->father = toRotate;
+    leftSon->father = toRotate->father;
+    if (toRotate->father == nil)
+        root = leftSon;
+    else if (toRotate == toRotate->father->left)
+        toRotate->father->left = leftSon;
+    else toRotate->father->right = leftSon;
+    leftSon->right = toRotate;
+    toRotate->father = leftSon;
+}
+
+/* Fixes the RB Tree property after the specified node has been inserted. */
+void RBInsertFixup(rankingNode *newNode) {
+    while (newNode != root && newNode->father->isRed) {
+        rankingNode *newNodeUncle;
+        if (newNode->father == newNode->father->father->left)
+            newNodeUncle = newNode->father->father->right;
+        else
+            newNodeUncle = newNode->father->father->left;
+        if (newNodeUncle->isRed) {
+            newNodeUncle->isRed = false;
+            newNode->father->isRed = false;
+            newNode->father->father->isRed = true;
+            newNode = newNode->father->father;
+        } else {
+            if (newNode->father == newNode->father->father->left &&
+                newNode == newNode->father->left) {
+                bool color = newNode->father->isRed;
+                newNode->father->isRed = newNode->father->father->isRed;
+                newNode->father->father->isRed = color;
+                rightRotate(newNode->father->father);
+            }
+            if (newNode->father == newNode->father->father->left &&
+                newNode == newNode->father->right) {
+                bool color = newNode->isRed;
+                newNode->isRed = newNode->father->father->isRed;
+                newNode->father->father->isRed = color;
+                leftRotate(newNode->father);
+                rightRotate(newNode->father->father);
+            }
+            if (newNode->father == newNode->father->father->right &&
+                newNode == newNode->father->right) {
+                bool color = newNode->father->isRed;
+                newNode->father->isRed = newNode->father->father->isRed;
+                newNode->father->father->isRed = color;
+                leftRotate(newNode->father->father);
+            }
+            if (newNode->father == newNode->father->father->right &&
+                newNode == newNode->father->left) {
+                bool color = newNode->isRed;
+                newNode->isRed = newNode->father->father->isRed;
+                newNode->father->father->isRed = color;
+                rightRotate(newNode->father);
+                leftRotate(newNode->father->father);
+            }
+        }
+    }
+    /* root must always be black */
+    root->isRed = false;
+}
+
+/* Inserts a new node inside the RB Tree. */
+void RBInsert(rankingNode *toInsert) {
+    rankingNode *previousNode = nil;
+    rankingNode *currentNode = root;
+    while (currentNode != nil) {
+        previousNode = currentNode;
+        if (toInsert->key < currentNode->key)
+            currentNode = currentNode->left;
+        else currentNode = currentNode->right;
+    }
+    toInsert->father = previousNode;
+    if (previousNode == nil)
+        root = toInsert;
+    else if (toInsert->key < previousNode->key)
+        previousNode->left = toInsert;
+    else previousNode->right = toInsert;
+    toInsert->left = nil;
+    toInsert->right = nil;
+    toInsert->isRed = true;
+    RBInsertFixup(toInsert);
+}
+
+/* Auxiliary method that fixes the RB tree after a deletion of a node, if the the deleted node was black. */
+void RBDeleteFixup(rankingNode *substitute) {
+    rankingNode *w;
+    while (substitute != root && !substitute->isRed) {
+        if (substitute == substitute->father->left) {
+            w = substitute->father->right;
+            if (w->isRed) {
+                w->isRed = false;
+                substitute->father->isRed = true;
+                leftRotate(substitute->father);
+                w = substitute->father->right;
+            }
+            if (!w->left->isRed && !w->right->isRed) {
+                w->isRed = true;
+                substitute = substitute->father;
+            } else if (!w->right->isRed) {
+                w->left->isRed = false;
+                w->isRed = true;
+                rightRotate(w);
+                w = substitute->father->right;
+            }
+            w->isRed = substitute->father->isRed;
+            substitute->father->isRed = false;
+            w->right->isRed = false;
+            leftRotate(substitute->father);
+            substitute = root;
+        } else {
+            w = substitute->father->left;
+            if (w->isRed) {
+                w->isRed = false;
+                substitute->father->isRed = true;
+                rightRotate(substitute->father);
+                w = substitute->father->left;
+            }
+            if (!w->right->isRed && !w->left->isRed) {
+                w->isRed = true;
+                substitute = substitute->father;
+            } else if (!w->left->isRed) {
+                w->right->isRed = false;
+                w->isRed = true;
+                leftRotate(w);
+                w = substitute->father->left;
+            }
+            w->isRed = substitute->father->isRed;
+            substitute->father->isRed = false;
+            w->left->isRed = false;
+            rightRotate(substitute->father);
+            substitute = root;
+        }
+    }
+    substitute->isRed = false;
+}
+
+/* Removes a node from the RB Tree. */
+void RBDelete(rankingNode *node) {
+    rankingNode *toDelete, *subTree;
+    if (node->left == nil || node->right == nil)
+        toDelete = node;
+    else toDelete = successor(node);
+    if (toDelete->left != nil)
+        subTree = toDelete->left;
+    else subTree = toDelete->right;
+    if (subTree != nil)
+        subTree->father = toDelete->father;
+    if (toDelete->father == nil)
+        root = subTree;
+    else if (toDelete == toDelete->father->left)
+        toDelete->father->left = subTree;
+    else toDelete->father->right = subTree;
+    if (toDelete != node) {
+        node->key = toDelete->key;
+    }
+    free(toDelete);
+    if (!node->isRed)
+        RBDeleteFixup(node);
+}
+
+void inputHandler() {
+}
+
+/* Initializes the program, setting dimension of graphs to classify and length of the ranking. */
+void initialize() {
+    scanf("%d %d", &graphDimension, &rankingLength);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    initialize();
+    inputHandler();
     return 0;
 }
